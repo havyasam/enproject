c9f89d521e0ac65d123161e30b009012fa5fcab6
havyas <havyasagaltha@gmail.com>
2024-01-14 10:52:44 +0530

    added css file

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var d=crypto.randomUUID();
+document.getElementById('myField').value = d
+
 function myfunction() {
    
-    var c=alert(d)
-    var e = prompt("Please enter your name", "Harry Potter");
+    var c=alert(d+"  <-- copy this link")
+    var e = prompt("Paste the link below");
+   
     if(d===e){
-      window.location.href = `https://chat1-ozya.onrender.com/`
+      window.location.href = `https://chat1-ozya.onrender.com/?id=${d}`
       
     }
     else{
@@ -12,17 +15,7 @@ function myfunction() {
     }
 
   }
-  function server(){
-    var e = prompt("Please enter your name");
-    if(d===e){
-      window.location.href = `https://sandeshhd.github.io/enchatt/chat/?id=${d}`
-      
-    }
-    else{
-      window.location.href =`https://javascript.info/`
-    }
-
-  }
+ 
   ScrollReveal({ 
     reset: true ,
     distance:'180px',
@@ -34,6 +27,45 @@ ScrollReveal().reveal('.main',{delay:100,origin:'bottom',interval:600});
 ScrollReveal().reveal('.first',{delay:100,origin:'bottom',interval:600});
 ScrollReveal().reveal('.second',{delay:100,origin:'bottom',interval:600});
 ScrollReveal().reveal('.third',{delay:100,origin:'bottom',interval:600});
+
+let popup =document.getElementById("popup")
+let makeblur =document.getElementById("makeblur")
+let main=doucment.getElementById("main")
+let first=doucment.getElementById("first")
+let header=document.getElementById("header")
+
+function openpopup(){
+  
+  popup.classList.add("open-popup")
+
+// makeblur.classList.add()
+ 
+//   first.classList.add("first-blur")
+//   header.classList.add("header-blur")
+
+
+}
+function closepopup(){
+  var e = prompt("Paste the link below");
+  var e = prompt("Paste the link below");
+   
+  if(d===e){
+    window.location.href = `https://chat1-ozya.onrender.com/?id=${d}`
+    
+  }
+  else{
+    window.location.href =`https://javascript.info/`
+  }
+
+ 
+}
+
+
+
+
+
+
+
   
  
     
